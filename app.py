20c4876e34423c7a2f662312af9f50eada400a1e
Wyatt School <wwebb@lakelandcollege.edu>
2022-11-10 22:21:45 -0600

    Database defined and connection works.

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask,request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:Password@localhost:3306/'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
