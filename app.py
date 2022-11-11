4d3e2e0eb89487d1af53f6be62da45db7e593b3c
Wyatt School <wwebb@lakelandcollege.edu>
2022-11-11 10:17:44 -0600

    Render posts and provide better feedback on blog creation input.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask,request, render_template, url_for
+from flask import Flask,request, render_template, session, url_for
 import flask
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -32,13 +33,35 @@ def alter_database(object):
         else:
             db.session.commit()
 
+def get_posts():
+    blogs = []
+    with app.app_context():
+        id_list = (db.session.query(Blog.id).all())
+        id_count = len(id_list)
+        #query = "SELECT id FROM blog"
+        #result = int(db.engine.execute(query))
+        for i in range(id_count):
+            query = "SELECT title FROM blog WHERE id=" + str(i)
+            post_title = str(db.engine.execute(query))
+            query = "SELECT body FROM blog WHERE id=" + str(i)
+            post_body = str(db.engine.execute(query))
+            blog_post = post_title + post_body
+            blogs.append(blog_post)
+        return blogs
+
 @app.route("/")
 def index():
     return flask.redirect(url_for("blog"))
 
 @app.route("/blog")
 def blog():
-    return render_template("blog.html")
+    blogs = get_posts()
+    display_posts = ""
+    for blog in blogs:
+        #post = """<h1>""" + post + """</h1>"""
+        display_posts += blog
+    return render_template("blog.html",
+    post = display_posts)
 
 @app.route('/newpost')
 def newpost():
@@ -48,11 +71,22 @@ def newpost():
 def add_blog():
     blog_title = request.form['title']
     blog_body = request.form['body']
+    if blog_title == "" or blog_title == " ":
+        feedback_message = "Please enter a title for your blog."
+        if blog_body == "" or blog_body == " ":
+            feedback_message = "Please enter a title for you blog and enter a body."
+        return render_template("blogform.html",
+        title = blog_title,
+        body = blog_body,
+        feedback = feedback_message)
+
     if blog_body == "" or blog_body == " ":
         feedback_message = "Please enter a body."
         return render_template("blogform.html",
         title = blog_title,
+        body = blog_body,
         feedback = feedback_message)
+    
     else:
         create_blog(blog_title, blog_body)
         return flask.redirect(url_for("blog"))
